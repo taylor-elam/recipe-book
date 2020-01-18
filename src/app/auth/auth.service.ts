aefe1beb78b79a51f989440be8668f513fafc677
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 17:34:38 -0600

    Added autoLogin and autoLogout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -22,9 +22,37 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private tokenExpirationTimer: any;
   user = new BehaviorSubject<User>(null);
 
   constructor(private http: HttpClient, private router: Router) { }
+
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirationDate: string
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
 
   login(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(LOGIN_URI + API_KEY,
@@ -44,6 +72,13 @@ export class AuthService {
   logout(): void {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+
+    if (this.tokenExpirationTimer != null) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+
+    this.tokenExpirationTimer = null;
   }
 
   signUp(email: string, password: string) {
@@ -65,6 +100,8 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000));
     const user           = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(errorResponse: HttpErrorResponse): Observable<AuthResponseData> {
