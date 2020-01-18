54b2b5c8d268775870d98684a0ab5c4a887d60dc
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 15:10:13 -0600

    Added routes and header navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RecipeComponent }       from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipeComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
