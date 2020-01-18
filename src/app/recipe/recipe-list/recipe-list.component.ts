a20fec076ed122877d42fdb2c358dc96845e151f
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 14:01:08 -0600

    Added Recipe model and displaying recipe list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
-  selector: 'app-recipe-list',
+  selector   : 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls  : ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Fish & Chips', 'Classic British fish & chips', 'https://bit.ly/2uL8q7o'),
+    new Recipe('Mac & Cheese', 'Ultra creamy mac and cheese', 'https://bit.ly/2uUgNOf')
+  ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
