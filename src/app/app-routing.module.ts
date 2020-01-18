8ce6672cca6749eaee66b5c845de2b7c42ac8938
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 17:52:27 -0600

    Created RecipesModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,24 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent }          from './auth/auth.component';
 import { AuthGuard }              from './auth/auth.guard';
-import { RecipeDetailComponent }  from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent }    from './recipe/recipe-edit/recipe-edit.component';
-import { RecipesResolverService } from './recipe/recipe-resolver.service';
-import { RecipeStartComponent }   from './recipe/recipe-start/recipe-start.component';
-import { RecipeComponent }        from './recipe/recipe.component';
 import { ShoppingListComponent }  from './shopping-list/shopping-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
-  {
-    path: 'recipes', component: RecipeComponent, canActivate: [AuthGuard], children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
-    ]
-  },
   { path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuard] }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
-import { BrowserModule }                    from '@angular/platform-browser';
-import { NgModule }                         from '@angular/core';
+import { FormsModule }                         from '@angular/forms';
+import { BrowserModule }                       from '@angular/platform-browser';
+import { NgModule }                            from '@angular/core';
 
 import { AppRoutingModule }        from './app-routing.module';
 import { AppComponent }            from './app.component';
 import { AuthInterceptorService }  from './auth/auth-interceptor.service';
 import { AuthComponent }           from './auth/auth.component';
 import { HeaderComponent }         from './header/header.component';
-import { RecipeComponent }         from './recipe/recipe.component';
-import { RecipeDetailComponent }   from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent }     from './recipe/recipe-edit/recipe-edit.component';
-import { RecipeListComponent }     from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent }     from './recipe/recipe-list/recipe-item/recipe-item.component';
-import { RecipeStartComponent }    from './recipe/recipe-start/recipe-start.component';
+import { RecipeModule }            from './recipe/recipe.module';
 import { DropdownDirective }       from './shared/dropdown.directive';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { ShoppingListComponent }   from './shopping-list/shopping-list.component';
@@ -27,21 +22,15 @@ import { ShoppingListService }     from './shopping-list/shopping-list.service';
     DropdownDirective,
     HeaderComponent,
     LoadingSpinnerComponent,
-    RecipeComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
+    ShoppingEditComponent
   ],
   imports     : [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    RecipeModule
   ],
   providers   : [
     ShoppingListService,
