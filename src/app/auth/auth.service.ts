7d24c011dd660358fd365b604bdc52f0be5a8e84
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 17:32:06 -0600

    Added logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse }           from '@angular/common/http';
 import { Injectable }                              from '@angular/core';
+import { Router }                                  from '@angular/router';
 import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError, tap }                         from 'rxjs/operators';
 
@@ -23,7 +24,7 @@ export interface AuthResponseData {
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   login(email: string, password: string): Observable<AuthResponseData> {
     return this.http.post<AuthResponseData>(LOGIN_URI + API_KEY,
@@ -38,6 +39,11 @@ export class AuthService {
         this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn);
       })
     );
+  }
+
+  logout(): void {
+    this.user.next(null);
+    this.router.navigate(['/auth']);
   }
 
   signUp(email: string, password: string) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -28,6 +28,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.dataStorageService.fetchRecipes().subscribe();
   }
 
+  onLogout(): void {
+    this.authService.logout();
+  }
+
   onSaveData(): void {
     this.dataStorageService.storeRecipes();
   }
