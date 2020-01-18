cc233b99c9ae6db51c3625a2809fcb78d143936f
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 14:34:47 -0600

    Updated RecipeItem component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls  : ['./app.component.scss']
 })
 export class AppComponent {
-  loadedFeature = '';
+  loadedFeature = 'recipe';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
