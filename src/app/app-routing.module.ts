32d0308b0b4fb13a2744de4b50e6c9fffa0ea27d
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 17:39:03 -0600

    Added authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent }          from './auth/auth.component';
+import { AuthGuard }              from './auth/auth.guard';
 import { RecipeDetailComponent }  from './recipe/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent }    from './recipe/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipe/recipe-resolver.service';
@@ -13,14 +14,14 @@ const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
   {
-    path: 'recipes', component: RecipeComponent, children: [
+    path: 'recipes', component: RecipeComponent, canActivate: [AuthGuard], children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
       { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
     ]
   },
-  { path: 'shopping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
