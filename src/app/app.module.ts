1528e06cc52559af7893b17d36da0b1bd55f4470
Taylor Elam <taylordeanelam@gmail.com>
2020-01-18 18:40:34 -0600

    Created AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { FormsModule }                         from '@angular/forms';
 import { BrowserModule }                       from '@angular/platform-browser';
 import { NgModule }                            from '@angular/core';
 
-import { AppRoutingModule }        from './app-routing.module';
-import { AppComponent }            from './app.component';
-import { AuthInterceptorService }  from './auth/auth-interceptor.service';
-import { AuthComponent }           from './auth/auth.component';
-import { HeaderComponent }         from './header/header.component';
-import { RecipeModule }            from './recipe/recipe.module';
-import { SharedModule }            from './shared/shared.module';
-import { ShoppingListModule }      from './shopping-list/shopping-list.module';
+import { AppRoutingModule }       from './app-routing.module';
+import { AppComponent }           from './app.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AuthModule }             from './auth/auth.module';
+import { HeaderComponent }        from './header/header.component';
+import { RecipeModule }           from './recipe/recipe.module';
+import { SharedModule }           from './shared/shared.module';
+import { ShoppingListModule }     from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
     HeaderComponent,
   ],
   imports     : [
@@ -23,6 +22,7 @@ import { ShoppingListModule }      from './shopping-list/shopping-list.module';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    AuthModule,
     RecipeModule,
     SharedModule,
     ShoppingListModule,
